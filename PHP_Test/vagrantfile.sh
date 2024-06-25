a40360dcefea9f79f0062f6c6857995ea1b3b2f7
Kiki Walter <walter.kiki@web4train.de>
2024-06-25 11:06:33 +0200

    update vagrantfile and vagrantfile.sh

--- PHP_Test/vagrantfile.sh
+++ PHP_Test/vagrantfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apachedir=/etc/apache2/sites-available
-confsite=phpgrundlagen.local
+confsite=phpkiki.local
 dokuRoot="public"
 
 sed -e "s/servername/$confsite/g" -e "s/public/$dokuRoot/g" ${apachedir}/example.conf  > ${apachedir}/vagrant.conf
