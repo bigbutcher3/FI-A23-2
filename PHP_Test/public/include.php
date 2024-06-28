a59625100bd0df675107a631feb5e2997f75de00
Kiki Walter <107316989+bigbutcher3@users.noreply.github.com>
2024-06-28 11:24:17 +0200

    start php work

---
+++ PHP_Test/public/include.php
@@ -0,0 +1,7 @@
+<?php
+include '../include/einbinden.html';
+include ('/vagrant/include/einbinden.html');
+echo __DIR__;
+echo '<br>';
+include __DIR__.'/../include/einbinden.html';
+include "index.html";
